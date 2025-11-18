32ef0d0e5fdddcf4e820cb4735b777888e1dceb5
VictorLux <mike_lux@hotmail.fr>
2025-11-18 16:35:56 +0100

    fix: Add @babel/register to transpile ES6 imports in main process
    - Fixes 'Cannot use import statement outside a module' error
    - Enables ES6 imports in Electron main process with Electron 22
    - Required after disabling electron-compile

--- config/main.js
+++ config/main.js
@@ -1,4 +1,9 @@
 // @flow
 
-// Direct loading without electron-compile
+// Use @babel/register to transpile ES6 imports in main process
+require('@babel/register')({
+  extensions: ['.js'],
+  cache: false,
+});
+
 require('./electron');
